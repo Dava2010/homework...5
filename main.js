0fdbc89ca5b2a868c363e64fb4ae61a5efe96ec1
Dava2010 <davlatbek20101106@gmail.com>
2024-11-18 18:46:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let space = {}
+
+space.name = prompt('Введите имя');
+space.id = prompt('Введите логин от аккаунта');
+space.password = prompt('Введите пароль от вашего аккаунта');
+
+console.log(space);
